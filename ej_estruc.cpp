d5187f885acecbb58bee1c966df962d50122eae6
julian_alse <julian.sanabria-b@mail.escuelaing.edu.co>
2025-02-11 15:33:13 -0500

    estructuras

---
+++ ej_estruc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+struct Point{
+    int x, y;
+};
+
+int main(){
+    Point p1 = {10, 20};
+    Point p2 = {-1,3};
+    p2.x =-5;
+    std:: cout << "Point P1(" << p1.x << ", " << p1.y << ")\n";
+    std:: cout << "Point P1(" << p2.x << ", " << p2.y << ")\n";
+    return 0;
+}
